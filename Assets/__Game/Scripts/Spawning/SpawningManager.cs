266518a30dbaf463c579e9b88664b693cbb6d25c
KennyGustavsson <kenny.gustavsson93@gmail.com>
2020-08-23 17:20:11 +0200

    Yellow clouds

---
+++ Assets/__Game/Scripts/Spawning/SpawningManager.cs
@@ -0,0 +1,26 @@
+﻿using UnityEngine;
+
+public class SpawningManager : MonoBehaviour
+{
+    public float maxDistance;
+
+    private Transform _player;
+
+    private void Awake()
+    {
+        _player = GameObject.Find("Player").transform;
+    }
+
+    private void DistanceCheck(Transform transform)
+    {
+        if(Vector3.Distance(_player.position, transform.position) > maxDistance)
+        {
+
+        }
+    }
+
+    private void Respawn()
+    {
+
+    }
+}
