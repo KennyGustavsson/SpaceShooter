a6ff0abbf0778e65ea06f69d6d5a237c899cffa5
KennyGustavsson <kenny.gustavsson93@gmail.com>
2020-08-23 20:33:28 +0200

    Fixed hooming missle so it wont explode on player

--- Assets/__Game/Scripts/Weapons/HoomingMissle.cs
+++ Assets/__Game/Scripts/Weapons/HoomingMissle.cs
@@ -69,7 +69,8 @@ namespace SS {
 
         private void OnTriggerEnter2D(Collider2D collision)
         {
-            if(collision.gameObject.layer != 8 || collision.gameObject.layer != 10) Explode();
+            if (collision.gameObject.layer == 8 || collision.gameObject.layer == 10) return;
+            Explode();
         }
 
         private IEnumerator Timer()
